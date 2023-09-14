48ee3b04996d1811e54ce9ba43e375841e8a6ff2
Michael Adawaren <gadocer54@gmail.com>
2023-09-14 19:35:37 +0100

    Changes to be committed: 	modified:   person/__pycache__/models.cpython-310.pyc 	modified:   person/models.py

--- person/models.py
+++ person/models.py
@@ -3,10 +3,10 @@ from pydantic import BaseModel, Field
 class Person(BaseModel):
     id: int = Field(default_factory=lambda: auto_id()) 
     name: str
-    age: int
+    
+    
     
 id_count = 0
-
 def auto_id():
     global id_count
     id_count += 1
